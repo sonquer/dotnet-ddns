c062d061859042189371e4b84beca24caa638187
Patryk Pasek <patrykpasek@onet.pl>
2022-08-27 19:44:01 +0200

    Providers refactor and Dockerfile update

---
+++ src/Openddns.Providers/Ovh/Models/DnsRecord.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Openddns.Providers.Ovh.Models
+{
+    internal class DnsRecord
+    {
+        [JsonProperty(PropertyName = "target")]
+        public string? Target { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public long Id { get; set; }
+
+        [JsonProperty(PropertyName = "fieldType")]
+        public string? FieldType { get; set; }
+
+        [JsonProperty(PropertyName = "zone")]
+        public string? Zone { get; set; }
+
+        [JsonProperty(PropertyName = "subDomain")]
+        public string? SubDomain { get; set; }
+
+        [JsonProperty(PropertyName = "ttl")]
+        public int Ttl { get; set; }
+    }
+}
